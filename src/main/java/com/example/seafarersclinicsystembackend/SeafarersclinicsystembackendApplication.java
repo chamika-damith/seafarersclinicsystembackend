09391d43c3652e9ae0f26e8530f80aa1ea4bda65
chamika-damith <chamikadamith9@gmail.com>
2025-02-01 13:15:34 +0530

    Initial commit

---
+++ src/main/java/com/example/seafarersclinicsystembackend/SeafarersclinicsystembackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.seafarersclinicsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeafarersclinicsystembackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeafarersclinicsystembackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seafarersclinicsystembackend/SeafarersclinicsystembackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seafarersclinicsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeafarersclinicsystembackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
